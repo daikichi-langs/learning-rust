733331994621d8f775cc821f1c0694718eed527e
daikichi-langs <105966658+daikichi-langs@users.noreply.github.com>
2022-09-09 03:31:47 +0900

    Delete lib-basic directory

--- lib-basic/src/generator.rs
+++
@@ -1,6 +0,0 @@
-use rand::Rng;
-pub fn gen_ran() {
-    let mut rng = rand::thread_rng();
-    let n: u8 = rng.gen(); // 0 ~ 255
-    n
-}
--- lib-basic/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod generator;
-pub fn print_random_number() {
-    let n = generator::gen_ran();
-    println!("Random u8: {}", n);
-}
